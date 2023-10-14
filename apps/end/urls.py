fed1dbec44577bda392e47ef7a3f50462326b5fd
didiotghima <nurlanuuulubeksultan@gmail.com>
2023-10-13 21:40:38 -0500

    end codex

--- apps/end/urls.py
+++ apps/end/urls.py
@@ -1,15 +1,8 @@
-# Импорты
 from django.urls import path
-from apps.mp3.views import MailFormView, done, error
+from apps.end.views import MailFormView, done, error
 
-# Определяем переменную urlpatterns - список URL-маршрутов для Django-приложения
 urlpatterns = [
-    # URL-маршрут для главной страницы
     path('', MailFormView.as_view(), name="index"),
-
-    # URL-маршрут для страницы "done"
     path('done/', done, name="done"),
-
-    # URL-маршрут для страницы "error"
     path('error/', error, name="error")
 ]
